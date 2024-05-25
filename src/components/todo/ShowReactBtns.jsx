8baa75cef17340207e8e4a24e10a7e8a99396be1
ritu-331 <161568483+ritu-331@users.noreply.github.com>
2024-05-25 00:45:43 -0700

    in one function three changes

--- src/components/todo/ShowReactBtns.jsx
+++ src/components/todo/ShowReactBtns.jsx
@@ -17,23 +17,31 @@ let tasks=[
 
 export default function ShowReactBtns() {
 const[listOfTasks,setListOfTasks]=useState(tasks)
-const onbtnclick =() =>{
-    setListOfTasks(tasks)
+const filter =(filterByText) =>{
+    switch (filterByText) {
+        case "All":
+            setListOfTasks(tasks)
+            break;
+            case "Completed":
+                setListOfTasks(tasks.filter((t) =>t.status))
+                break;
+                case "Incompleted":
+                    setListOfTasks(tasks.filter((t) => !t.status))
+                    break;
+    
+        default:
+            setListOfTasks(tasks)
+            break;
+    }
 }
 
 
   return (
    <>
     <div className='flex'> 
- <div onClick={onbtnclick}><TodoFormBtns btntext="All" color="blue"/></div>
- <div onClick={() =>{
-    let filteredList= tasks.filter((t) => t.status)
-    setListOfTasks(filteredList)
- }}><TodoFormBtns btntext="Completed" color="green"/></div>
- <div onClick={() =>{
-    let filteredList = tasks.filter((t) =>!t.status)
-    setListOfTasks(filteredList)
- }}><TodoFormBtns btntext="Incompleted" color="red"/></div>
+ <div onClick={()=>filter("All")}><TodoFormBtns btntext="All" color="blue"/></div>
+ <div onClick={()=>filter("Completed")}><TodoFormBtns btntext="Completed" color="green"/></div>
+ <div onClick={() =>filter("Incompleted")}><TodoFormBtns btntext="Incompleted" color="red"/></div>
  </div>
  <TodoReactBtn data={listOfTasks}/>
    </>
